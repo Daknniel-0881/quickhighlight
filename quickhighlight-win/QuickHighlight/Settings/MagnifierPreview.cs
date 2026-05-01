a6502f4a3bdc07d2e7e07f3d6f1f6f901564061c
Daknniel <huangzhihao0881@gmail.com>
2026-05-01 20:18:01 +0800

    fix: qualify WPF preview drawing types

--- quickhighlight-win/QuickHighlight/Settings/MagnifierPreview.cs
+++ quickhighlight-win/QuickHighlight/Settings/MagnifierPreview.cs
@@ -1,5 +1,8 @@
 using System.Windows;
 using System.Windows.Media;
+using MediaColor = System.Windows.Media.Color;
+using MediaPen = System.Windows.Media.Pen;
+using WpfPoint = System.Windows.Point;
 
 namespace QuickHighlight.Settings;
 
@@ -14,20 +17,20 @@ public sealed class MagnifierPreview : FrameworkElement
         }
 
         var bounds = new Rect(0, 0, ActualWidth, ActualHeight);
-        var bg = new LinearGradientBrush(Color.FromRgb(245, 246, 248), Color.FromRgb(218, 222, 230), 45);
+        var bg = new LinearGradientBrush(MediaColor.FromRgb(245, 246, 248), MediaColor.FromRgb(218, 222, 230), 45);
         dc.DrawRectangle(bg, null, bounds);
 
         for (var x = 0.0; x < ActualWidth; x += 18)
         {
-            dc.DrawLine(new Pen(new SolidColorBrush(Color.FromArgb(28, 0, 0, 0)), 0.5), new Point(x, 0), new Point(x, ActualHeight));
+            dc.DrawLine(new MediaPen(new SolidColorBrush(MediaColor.FromArgb(28, 0, 0, 0)), 0.5), new WpfPoint(x, 0), new WpfPoint(x, ActualHeight));
         }
 
         for (var y = 0.0; y < ActualHeight; y += 18)
         {
-            dc.DrawLine(new Pen(new SolidColorBrush(Color.FromArgb(28, 0, 0, 0)), 0.5), new Point(0, y), new Point(ActualWidth, y));
+            dc.DrawLine(new MediaPen(new SolidColorBrush(MediaColor.FromArgb(28, 0, 0, 0)), 0.5), new WpfPoint(0, y), new WpfPoint(ActualWidth, y));
         }
 
-        var center = new Point(ActualWidth / 2, ActualHeight / 2);
+        var center = new WpfPoint(ActualWidth / 2, ActualHeight / 2);
         var width = settings.Shape == MagnifierShape.Circle
             ? Math.Min(settings.Radius, Math.Min(ActualWidth, ActualHeight) / 2 - 8) * 2
             : Math.Min(settings.RectWidth * 0.45, ActualWidth - 18);
@@ -42,7 +45,7 @@ public sealed class MagnifierPreview : FrameworkElement
         var donut = new GeometryGroup { FillRule = FillRule.EvenOdd };
         donut.Children.Add(new RectangleGeometry(bounds));
         donut.Children.Add(lensGeometry);
-        dc.DrawGeometry(new SolidColorBrush(Color.FromArgb((byte)(settings.DimAlpha * 255), 0, 0, 0)), null, donut);
+        dc.DrawGeometry(new SolidColorBrush(MediaColor.FromArgb((byte)(settings.DimAlpha * 255), 0, 0, 0)), null, donut);
 
         dc.PushClip(lensGeometry);
         dc.PushTransform(new ScaleTransform(settings.Zoom, settings.Zoom, center.X, center.Y));
@@ -54,14 +57,14 @@ public sealed class MagnifierPreview : FrameworkElement
             18,
             Brushes.Black,
             VisualTreeHelper.GetDpi(this).PixelsPerDip);
-        dc.DrawText(text, new Point(center.X - text.Width / 2, center.Y - text.Height / 2));
+        dc.DrawText(text, new WpfPoint(center.X - text.Width / 2, center.Y - text.Height / 2));
         dc.Pop();
         dc.Pop();
 
         if (settings.ShowRing)
         {
-            dc.DrawGeometry(null, new Pen(Brushes.White, 2), lensGeometry);
-            dc.DrawGeometry(null, new Pen(new SolidColorBrush(Color.FromArgb(90, 0, 0, 0)), 1), lensGeometry);
+            dc.DrawGeometry(null, new MediaPen(Brushes.White, 2), lensGeometry);
+            dc.DrawGeometry(null, new MediaPen(new SolidColorBrush(MediaColor.FromArgb(90, 0, 0, 0)), 1), lensGeometry);
         }
     }
 }
